9e088e805388ac77827f13db0e4a30939853df37
InnocentEdem <80456476+Quessi@users.noreply.github.com>
2023-08-22 11:59:49 +0000

    update

---
+++ src/TodoItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function TodoItem() {
+    //receive and display the todotext props from Todos.js
+  return (
+    <div></div>
+  )
+}
+
+export default TodoItem
---
+++ src/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todos() {
+    //Display an input element with a button to enter and save the entered text
+    //Display a list of todos entered by the user, using the TodoItem component
+  return (
+    <div></div>
+  )
+}
+
+export default Todos
